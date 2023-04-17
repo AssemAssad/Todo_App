dcdff7ae9351296ef5d3297b423b88015e2fd2d0
AssemAssad <101038782+AssemAssad@users.noreply.github.com>
2023-04-17 17:39:02 +0200

    commit 1

--- lib/todo_app/views/screens/login/splash_screen.dart
+++ lib/todo_app/views/screens/login/splash_screen.dart
@@ -10,7 +10,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     initFun();
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: FlutterLogo(),
       ),
